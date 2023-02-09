2cf3ed4335ee79be7018d6a8a803ff28442c33df
Cristian <crishzz01@gmail.com>
2023-02-08 20:36:10 -0500

    Desarollo del ejercicio numero 1

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    Name=input("Please enter your name: ")
+    Age=int(input("Please enter your age: "))
+
+    print(f"Hello! {Name} you are {Age} years old, welcome to the system")
+
+
+
+
+
