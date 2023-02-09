0dbbced6326c38283da93b197d78dd36eb3758f8
Cristian <crishzz01@gmail.com>
2023-02-08 23:29:30 -0500

    Este es el archivo ejecutable en donde se tendran acceso a los 15 ejercicios facilmente

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import Ejercicios.Ejercicio1
 import Ejercicios.ejercicio2
+import Ejercicios.ejercicio3
+import Ejercicios.ejercicio4
+import Ejercicios.ejercicio5
+import Ejercicios.ejercicio6
+import Ejercicios.ejercicio7
+import Ejercicios.ejercicio8
+import Ejercicios.ejercicio9
+import Ejercicios.ejercicio10
+import Ejercicios.ejercicio11
+import Ejercicios.ejercicio12
+import Ejercicios.ejercicio13
+import Ejercicios.ejercicio14
+import Ejercicios.ejercicio15
+
