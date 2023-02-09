83463cc32fa15de2160a82dab947487a8986c664
Cristian <crishzz01@gmail.com>
2023-02-08 21:50:31 -0500

    Desarollo del ejercicio numero 2 area del circulo con radio

--- Ejercicios/ejercicio2.py
+++ Ejercicios/ejercicio2.py
@@ -1,9 +1,9 @@
 if __name__ == "__main__":
     def areacirculo():
-        Radio = float(input("Please enter the radio of the circle: "))
-        Area = 3.1416 * Radio**2
-        print(f"The area of the circle is: {Area}")
-        return Area
+        radio = float(input("Please enter the radio of the circle: "))
+        area = 3.1416 * radio**2
+        print(f"The area of the circle is: {area}")
+        return area
 
 areacirculo()
 
