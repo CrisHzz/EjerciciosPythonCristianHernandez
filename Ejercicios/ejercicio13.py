7bd4d3a96c69dbe57607292d3410f6996b2a6744
Cristian <crishzz01@gmail.com>
2023-02-08 23:08:00 -0500

    Desarollo del ejercicio numero 13 con 2 numeros realizar suma , multiplicacion ,division y resta

---
+++ Ejercicios/ejercicio13.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    print("Welcome to the calculator")
+    number_1 = float(input("Please enter the first number: "))
+    number_2 = float(input("Please enter the second number: "))
+    print("the sum of the numbers is: ", number_1 + number_2)
+    print("the subtraction of the numbers is: ", number_1 - number_2)
+    print("the multiplication of the numbers is: ", number_1 * number_2)
+    print("the division of the numbers is: ", number_1 / number_2)
