9ce64aa36683d5e216a930e40199fcca480e2709
Cristian <crishzz01@gmail.com>
2023-02-08 21:49:35 -0500

    Desarollo del ejercicio numero 1 nombre y edad

--- Ejercicios/Ejercicio1.py
+++ Ejercicios/Ejercicio1.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
-    Name=input("Please enter your name: ")
-    Age=int(input("Please enter your age: "))
+    name=input("Please enter your name: ")
+    age=int(input("Please enter your age: "))
 
-    print(f"Hello! {Name} you are {Age} years old, welcome to the system")
+    print(f"Hello! {name} you are {age} years old, welcome to the system")
 
 
 
